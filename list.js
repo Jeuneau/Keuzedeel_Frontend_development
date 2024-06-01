a617645b11a8f6849a2fece130fab0259fb72326
Jeuneau <boekhoudtjeuneau@gmail.com>
2024-06-01 18:14:53 +0200

    lists functional

--- list.js
+++ list.js
@@ -1,52 +1,64 @@
-function addWatchedAnime(animeValue) {
-    var ul = document.getElementById("watchedAnime");
-    var anime = document.getElementById("watchedInput");
+window.onload = function() {
+    loadList('watchlist', 'animeWatchlist');
+    loadList('watched', 'watchedAnime');
+};
+
+function loadList(listName, listId) {
+    var list = JSON.parse(localStorage.getItem(listName)) || [];
+    var ul = document.getElementById(listId);
+    ul.innerHTML = ''; // Clear the list
+    list.forEach(function(anime) {
+        addToList(ul, list, anime, listName);
+    });
+}
+
+function addToList(ul, list, anime, listName) {
     var li = document.createElement("li");
-    li.appendChild(document.createTextNode(animeValue || anime.value));
+    li.appendChild(document.createTextNode(anime));
     var removeBtn = document.createElement("button");
     removeBtn.textContent = "Remove";
     removeBtn.onclick = function() {
         this.parentNode.remove();
-        // Remove from local storage
-        var watchedAnime = JSON.parse(localStorage.getItem('watchedAnime')) || [];
-        var index = watchedAnime.indexOf(animeValue || anime.value);
-        if (index !== -1) watchedAnime.splice(index, 1);
-        localStorage.setItem('watchedAnime', JSON.stringify(watchedAnime));
+        var index = list.indexOf(anime);
+        if (index !== -1) {
+            list.splice(index, 1);
+            localStorage.setItem(listName, JSON.stringify(list));
+        }
     };
     li.appendChild(removeBtn);
     ul.appendChild(li);
-    // Add to local storage
-    if (!animeValue) {
-        var watchedAnime = JSON.parse(localStorage.getItem('watchedAnime')) || [];
-        watchedAnime.push(anime.value);
-        localStorage.setItem('watchedAnime', JSON.stringify(watchedAnime));
-        anime.value = "";
-    }
 }
 
-function addToWatchlist(animeValue) {
-    var ul = document.getElementById("animeWatchlist");
-    var anime = document.getElementById("watchlistInput");
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode(animeValue || anime.value));
-    var removeBtn = document.createElement("button");
-    removeBtn.textContent = "Remove";
-    removeBtn.onclick = function() {
-        this.parentNode.remove();
-        // Remove from local storage
-    };
-    li.appendChild(removeBtn);
-    ul.appendChild(li);
-    // Add to local storage
-    if (!animeValue) {
-        var animeWatchlist = JSON.parse(localStorage.getItem('animeWatchlist')) || [];
-        animeWatchlist.push(anime.value);
-        localStorage.setItem('animeWatchlist', JSON.stringify(animeWatchlist));
-        anime.value = "";
+document.getElementById('watchlistInput').addEventListener('change', function() {
+    var list = JSON.parse(localStorage.getItem('watchlist')) || [];
+    var anime = this.value;
+    if (anime && !list.includes(anime)) {
+        list.push(anime);
+        localStorage.setItem('watchlist', JSON.stringify(list));
+        addToList(document.getElementById('animeWatchlist'), list, anime, 'watchlist');
     }
+});
+
+document.getElementById('watchedInput').addEventListener('change', function() {
+    var list = JSON.parse(localStorage.getItem('watched')) || [];
+    var anime = this.value;
+    if (anime && !list.includes(anime)) {
+        list.push(anime);
+        localStorage.setItem('watched', JSON.stringify(list));
+        addToList(document.getElementById('watchedAnime'), list, anime, 'watched');
+    }
+});
+
+document.getElementById('clearWatchlist').addEventListener('click', function() {
+    clearList('watchlist', 'animeWatchlist');
+});
+
+document.getElementById('clearWatchedAnime').addEventListener('click', function() {
+    clearList('watched', 'watchedAnime');
+});
+
+function clearList(listName, listId) {
+    localStorage.removeItem(listName);
+    var ul = document.getElementById(listId);
+    ul.innerHTML = '';
 }
-
-
-
-
-
